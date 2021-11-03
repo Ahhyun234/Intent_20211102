2dbb614473efed68c1e2d7306f53fcc7b56d8506
이아현 <suk03029@gmail.com>
2021-11-03 10:18:13 +0900

    닉네임 변경 화면에서 확인 버튼에 대한 처리 1. 입력한 값 저장 2. 인텐트에 값 첨부 3. 결과 설정(확인,intent) 4. finish()

--- app/src/main/java/com/neppplus/intent_20211102/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211102/EditNicknameActivity.kt
@@ -2,10 +2,30 @@ package com.neppplus.intent_20211102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnOK.setOnClickListener {
+
+//            1. 입력한 닉네임 변수에 저장
+            val inpitNickName = edtNewNickname.text.toString()
+//            1. 닉네임 들고가야 함 -> 결과를 담아줘야 함 -> 인텐트 추가 활용
+//            이전에 왔다 간다는 왕복을 이야기 해줬기 때문에 출발 도착지를 적지 않음
+            val resultIntent=intent()
+
+//            실제 데이터 첨부, putExtra()
+            resultIntent.putExtra("nick",inpitNickName)
+
+//            최종 결과 (확인을 눌렀을 때, 들고 돌아갈 데이터 지정)
+            setResult(RESULT_OK, resultIntent)
+
+//            실제 이전 화면으로 돌아가는 것
+            finish()
+
+        }
     }
 }
